ef2bc7cd55b475ded4b7ab995bc06cfef24bec94
Anton Belyaev <belyaev@gifterycard.ru>
2016-02-09 19:12:17 +0300

    Добавлен метод для установки нескольких свойств в объектах-наследниках RequestData

--- src/classes/data/RequestData.php
+++ src/classes/data/RequestData.php
@@ -9,6 +9,25 @@ namespace Giftery\classes\data;
 abstract class RequestData
 {
 	/**
+	 * Метод для установки нескольких свойств через массив ключ-значение. Для каждого элемента массива будет вызван
+	 * соответствующий ему метод (например, элементу order_id будет вызван метод setOrderId), если метод не существует
+	 * - выбрасывается исключение.
+	 * @param array $properties
+	 */
+	public function set(array $properties)
+	{
+		foreach ($properties as $name => $value) {
+			$method = 'set' . str_replace('_', '', $name);
+
+			if (!method_exists($this, $method)) {
+				throw new \BadMethodCallException('Не найден метод ' . $method);
+			}
+
+			call_user_func([$this, $method], $value);
+		}
+	}
+
+	/**
 	 * @return string
 	 */
 	public function toJson()
