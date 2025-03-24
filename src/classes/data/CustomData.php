1e2d5603f06b778a571aad62e4e8573338e72f64
Nik Dm <nox@nox.kz>
2025-03-24 20:20:24 +0300

    fix: Creation of dynamic property is deprecated

--- src/classes/data/CustomData.php
+++ src/classes/data/CustomData.php
@@ -3,29 +3,23 @@
 namespace Giftery\classes\data;
 
 /**
- * Class GetProductsData
+ * Class CustomData
  * @package Giftery\classes\data
  */
 class CustomData extends RequestData
 {
-    public function set(array $properties)
+    private array $properties = [];
+
+    public function set(array $properties): void
     {
         foreach($properties as $key => $val)
         {
-            $this->{$key} = $val;
+            $this->properties[$key] = $val;
         }
     }
 
     public function __toString()
     {
-        $result = [];
-        $fields = (array)$this;
-
-        foreach ($fields as $key => $value)
-        {
-            $result[$key] = $value;
-        }
-
-        return json_encode($result);
+        return json_encode($this->properties);
     }
 }
